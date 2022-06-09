9ba7dfcc3911593652dd683193627d11c8246e59
maximilian <maximilian.stefan.ernst@gmail.com>
2022-06-09 17:24:00 +0200

    add main module

---
+++ src/AutoDiffSEM.jl
@@ -0,0 +1,10 @@
+module AutoDiffSEM
+
+    using StructuralEquationModels
+    # import StructuralEquationModels: print_type_name, print_field_types, sem_fit, start_val
+
+    include("AutoDiffSEM.jl")
+
+    export 
+
+end
